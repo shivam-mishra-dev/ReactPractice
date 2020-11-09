1027161e867bec2b73c80bb81697eb1c02e5315e
shivam <shivamkm31@gmail.com>
2020-11-09 15:14:34 +0530

    FInal File 9

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 var NotFound = () =>{
     return(
         <div className="notFound">
-         <h2 className="display-1">Page Not Found</h2>
+         <h2 className="display-1">notfound component Page Not Found</h2>
         </div>
     );
 }
